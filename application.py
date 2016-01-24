b48f9c794275a1a481718967f7388510877809e7
Emma Ferguson <emmamferguson@gmail.com>
2016-01-24 10:28:59 -0800

    webpages both show up when run locally; data transfer from form not working yet

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,10 +9,35 @@ def index_page():
 
     return "<html><body>This is the homepage.</body></html>"
 
-    # Alternately, we could make this a Jinja template in `templates/`
-    # and return that result of rendering this, like:
-    #
-    # return render_template("index.html")
+@app.route("/application-form", methods=['GET', 'POST'])
+def application_form():
+    """Show application form."""
+
+    form = request.form
+    print form
+    print type(form)
+
+    firstname = request.form.get("first-name", "Robin")
+    lastname = request.form.get("last-name", "Smith")
+    salary = request.form.get("salary", "65000")
+    position = request.form.get("position", "Product Manager")
+
+    return render_template("application-form.html",
+                            firstname = firstname,
+                            lastname = lastname,
+                            salary = salary,
+                            position = position)
+
+@app.route("/application-response")
+def application_response():
+    """Show summary of responses from application-form."""
+
+    # return render_template("application-response.html")
+    return render_template("application-response.html")
+
+                            
+    
+
 
 
 if __name__ == "__main__":
